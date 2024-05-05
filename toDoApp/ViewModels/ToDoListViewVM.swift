fd70a17758919e8d8fcd6165f4bea128278dfd21
ariishaat <92178520+ariishaat@users.noreply.github.com>
2024-05-05 16:27:10 -0400

    adding new item view + button function

---
+++ toDoApp/ViewModels/ToDoListViewVM.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoListViewVM.swift
+//  toDoApp
+//
+//  Created by Arika I on 2024-05-05.
+//
+
+import Foundation
---
+++ toDoApp/Views/ToDoListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListView.swift
+//  toDoApp
+//
+//  Created by Arika I on 2024-05-05.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
