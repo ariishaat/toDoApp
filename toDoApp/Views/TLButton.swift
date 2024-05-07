bd386b5fbabbb2bab534eac4258e2541c2dfa973
ariishaat <92178520+ariishaat@users.noreply.github.com>
2024-05-06 22:52:02 -0400

    fixes

---
+++ toDoApp/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  toDoApp
+//
+//  Created by Arika I on 2024-05-06.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
