7f0edf0cadd26685c9c6cf49c502e32b5800e2bd
ariishaat <92178520+ariishaat@users.noreply.github.com>
2024-05-08 15:58:06 -0400

    new fixes

---
+++ toDoApp/Views/ToDoItemVIew.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItemVIew.swift
+//  toDoApp
+//
+//  Created by Arika I on 2024-05-08.
+//
+
+import SwiftUI
+
+struct ToDoItemVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoItemVIew()
+}
